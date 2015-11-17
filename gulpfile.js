aa1331b7c0702a9b01f53f1e1a80abe0d8714846
Eric McCormick <edm00se@gmail.com>
2015-11-17 19:45:27 +0000

    gulpfile.js updated for browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,7 @@ var gulp      = require('gulp'),
   jsonServer  = require('gulp-json-srv'),
   minify      = require('gulp-minify-css'),
   rename      = require('gulp-rename'),
+  minifyHTML  = require('gulp-minify-html'),
   server      = jsonServer.start({  // config the json-server instance
           data: 'db.json',
           id: 'unid',
@@ -19,28 +20,29 @@ var gulp      = require('gulp'),
             "/xsp/:characters/:id": "/:characters/:id"
           },
           deferredStart: true
-        });
+        }),
+  browserSync   = require('browser-sync').create();
 
 // configure the jshint task
 gulp.task('jshint', function() {
-  return gulp.src('NSF/WebContent/js/*.js')
+  return gulp.src('./NSF/WebContent/js/*.js')
     .pipe(jshint())
     .pipe(jshint.reporter('jshint-stylish'));
 });
 
 // build dist JS assets
 gulp.task('build-js', function() {
-  return gulp.src('NSF/WebContent/js/*.js')
+  return gulp.src('./NSF/WebContent/js/*.js')
     .pipe(sourcemaps.init())
     .pipe(concat('scripts_from_gulp.js'))
     //only uglify if gulp is ran with '--type production'
     .pipe(gutil.env.type === 'production' ? uglify() : gutil.noop()) 
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest('public/dist'));
+    .pipe(gulp.dest('./public/dist'));
 });
 
 gulp.task('cssmin', function(){
-  gulp.src('NSF/WebContent/css/*.css')
+  gulp.src('./NSF/WebContent/css/*.css')
     .pipe(minify({ keepBreaks: false }))
     /*
     // builds individually minified files
@@ -52,14 +54,25 @@ gulp.task('cssmin', function(){
     .pipe(gulp.dest('public/dist'));
 });
 
+gulp.task('minify-html', function(){
+  var opts = {
+    conditionals: true,
+    spare: true
+  };
+
+  return gulp.src('./NSF/WebContent/partials/*.html')
+    .pipe(minifyHTML(opts))
+    .pipe(gulp.dest('./public/dist'));
+});
+
 // configure which files to watch and what tasks to use on file changes
 gulp.task('watch', function() {
-  gulp.watch('NSF/WebContent/js/*.js', ['jshint']);
+  gulp.watch('./NSF/WebContent/js/*.js', ['jshint']);
   gulp.watch(['db.json'], function(){
     server.reload();
   });
-  gulp.watch('NSF/WebContent/css/*.css', ['cssmin']);
-  gulp.watch('NSF/WebContent/partials/*.html')
+  gulp.watch('./NSF/WebContent/css/*.css', ['cssmin']);
+  gulp.watch('./NSF/WebContent/partials/*.html', ['minify-html'])
 });
 
 // starts the json-server instance
@@ -67,5 +80,15 @@ gulp.task('serverStart', function(){
   server.start();
 });
 
+// loading browser-sync as a proxy, must load after json-server
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        proxy: "http://localhost:3000/"
+    });
+});
+
+// generic build, assuming we don't want the preview
+gulp.task('build', ['jshint', 'build-js', 'cssmin', 'minify-html']);
+
 // define the default task and add the watch task to it
-gulp.task('default', ['watch','serverStart']);
+gulp.task('default', ['watch','serverStart','browser-sync']);
